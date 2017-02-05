3fbc6b9627d7c24f1108b85ce07d3faac64d9b39
dfsklar <dfsklar@gmail.com>
2017-02-05 18:55:33 -0500

    getting somewhere

--- PROJautorepairLargeReconcFailures/restore_entitlements.py
+++ PROJautorepairLargeReconcFailures/restore_entitlements.py
@@ -5,12 +5,18 @@ import pickle
 dict_workspaces = pickle.load(open('workspaces.pkl', 'rb'))
 dict_broles = pickle.load(open('broles.pkl', 'rb'))
 dict_ents = pickle.load(open('ents.pkl', 'rb'))
+set_redundant = pickle.load(open('manifests_ambiguous.pkl', 'rb'))
 
+set_inuse_ambiguous = set()
 
 with open('Result of IDM reconcile based on WS-5139.csv', 'rU') as csvfile:
     reader = csv.reader(csvfile)
     linenum = 0
-    ignorenum = 0
+    errcounts = {
+        'unknown_role': 0,
+        'unknown_manifest': 0,
+        'ambiguous_manifest': 0
+        }
     oknum = 0
     for row in reader:
         linenum += 1
@@ -27,11 +33,21 @@ with open('Result of IDM reconcile based on WS-5139.csv', 'rU') as csvfile:
             if action == 'Remove' and obj == 'Entitlement':
                 subpr = int(row[4])
                 if not dict_broles.has_key(rolename):
+                    errcounts['unknown_role'] += 1
                     print "On line %d - unknown role %s" % (linenum, rolename)
                 elif not dict_ents.has_key(ent):
+                    errcounts['unknown_manifest'] += 1
                     print "On line %d - unknown entsummary: %s" % (linenum, ent)
+                elif ent in set_redundant:
+                    set_inuse_ambiguous.add(ent)
+                    errcounts['ambiguous_manifest'] += 1
+                    print "On line %d - entsummary is ambiguous: %s" % (linenum, ent)
                 else:
                     oknum +=1
 
-print "Actual number we could actually restore: %d" % oknum
-                    
+print "Number of entitlements we can restore without ambiguity: %d" % oknum
+print "Number we cannot restore with full certainty: " + str(errcounts)
+
+print '. . . . .'
+print "Number of detected ambiguous manifests: %d" % len(set_redundant)
+print "Number of actually-in-use ambiguous manifests: %d" % len(set_inuse_ambiguous)
