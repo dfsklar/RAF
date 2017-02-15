8b709c87d5dd4f3c4bf58ddd440d0fbba8b29745
David Sklar <dfsklar@gmail.com>
2017-02-14 22:15:22 -0500

    version bump to 17.02.03

--- Site1.Master.cs
+++ Site1.Master.cs
@@ -15,7 +15,7 @@ namespace _6MAR_WebApplication
   {
 
     //Version info:
-    static public string AFWACRELNUM = "R-AF release 17.02.01";
+    static public string AFWACRELNUM = "RAF version 17.02.03";
 
 
     protected AFWACsession THESESSION = null;
